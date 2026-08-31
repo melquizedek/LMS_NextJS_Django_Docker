054751655e92ac0b361f7a37997ed662629824cb
Melquizedek Paala <melquizedekpaala@Admins-MacBook-Pro.local>
2026-08-31 12:34:34 +0800

    use CompressedStaticFilesStorage to avoid manifest volume mismatch

--- olap_backend_api/config/settings/base.py
+++ olap_backend_api/config/settings/base.py
@@ -222,7 +222,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
